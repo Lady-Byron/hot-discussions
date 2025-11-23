fe9533d60e5229bea571bedb32726015fe93e0d5
Lady-Byron <samantha-everett@outlook.com>
2025-11-23 03:41:35 -0500

    Update hotDiscussionsWidget.js

--- js/src/common/components/hotDiscussionsWidget.js
+++ js/src/common/components/hotDiscussionsWidget.js
@@ -1,75 +1,91 @@
-/*
- * This file is part of Hot Discussions Widget.
- *
- * Copyright (c) 2021 Marco Colia.
- * https://flarum.it
- *
- * For the full copyright and license information, please view the LICENSE.md
- * file that was distributed with this source code.
- */
-
 import Widget from 'flarum/extensions/afrux-forum-widgets-core/common/components/Widget';
 import app from 'flarum/forum/app';
 import LoadingIndicator from 'flarum/common/components/LoadingIndicator';
-import Link from "flarum/common/components/Link";
+import Link from 'flarum/common/components/Link';
+import icon from 'flarum/common/helpers/icon';
 
-export default class hotDiscussionsWidget extends Widget {
-    oninit(vnode) {
-        super.oninit(vnode);
-        this.loading = true;
-    }
+export default class HotDiscussionsWidget extends Widget {
+  oninit(vnode) {
+    super.oninit(vnode);
+    this.loading = true;
+  }
 
-    oncreate(vnode) {
-        const limitHotDisc = app.forum.attribute('justoverclock-hot-discussions.discLimit') || 10;
-        const filterDiscussions = app.forum.attribute('justoverclock-hot-discussions.hideClosed') === "1" ? '-is:locked' : '';
-        // get discussions object
-        const hotDisc = app.store
-            .find('discussions', {
-                sort: '-commentCount',
-                filter: { q: filterDiscussions }, // Exclude closed discussions
-                page: { limit: limitHotDisc },
-            })
-            .then((results) => {
-                this.hotDisc = results;
-                this.loading = false;
-                m.redraw();
-            });
-    }
+  oncreate(vnode) {
+    super.oncreate(vnode);
+    
+    const limitHotDisc = app.forum.attribute('justoverclock-hot-discussions.discLimit') || 10;
+    const hideClosed = app.forum.attribute('justoverclock-hot-discussions.hideClosed') === '1';
+    const filterParams = hideClosed ? { q: '-is:locked' } : {};
 
-    className() {
-        // css class for the container
-        return 'hotDiscussions-widget';
-    }
+    // 核心逻辑保留：多获取数据以防过滤损耗
+    const fetchLimit = limitHotDisc * 3;
 
-    icon() {
-        // Widget icon.
-        return 'fab fa-hotjar hotwdg';
-    }
+    app.store
+      .find('discussions', {
+        sort: '-commentCount',
+        filter: filterParams,
+        page: { limit: fetchLimit },
+        // include 'state' 修复阅读进度跳转
+        // include 'tags' 用于过滤隐藏标签
+        include: 'tags,state', 
+      })
+      .then((results) => {
+        // 过滤隐藏标签
+        const visibleDiscussions = results.filter((disc) => {
+          const tags = disc.tags();
+          if (!tags) return true;
+          return !tags.some((tag) => tag.subscription() === 'hide');
+        });
 
-    title() {
-        // Widget title.
-        return app.translator.trans('justoverclock-hot-discussions.forum.widget-title');
-    }
+        this.hotDisc = visibleDiscussions.slice(0, limitHotDisc);
+        this.loading = false;
+        m.redraw();
+      });
+  }
 
-    content() {
-        if (this.loading) {
-            return <LoadingIndicator />;
-        }
-        return (
-            <div className="hotDiscussion-content">
-                <ul className="hotDisc fa-ul">
-                    {this.hotDisc.map((disc) => {
-                        return (
-                            <li class="hotDiscWidget">
-                                <i class="fas fa-caret-right hotdiscIcon"></i>
-                                <Link href={app.route.discussion(disc)} alt={disc.title()} className="hotDiscLink">
-                                   {disc.title()}
-                                </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
-            </div>
-        );
+  className() {
+    return 'hotDiscussions-widget';
+  }
+
+  icon() {
+    // 保持原有图标，但去掉了强制红色的类
+    return 'fab fa-hotjar'; 
+  }
+
+  title() {
+    return app.translator.trans('justoverclock-hot-discussions.forum.widget-title');
+  }
+
+  content() {
+    if (this.loading) {
+      return <LoadingIndicator />;
     }
+    
+    return (
+      <div className="hotDiscussion-content">
+        <ul className="hotDisc-list">
+          {this.hotDisc.map((disc) => {
+            return (
+              <li className="hotDisc-item">
+                <Link href={app.route.discussion(disc)} className="hotDisc-link">
+                  
+                  {/* 左侧：简单的序号或图标装饰，这里用一个小圆点装饰 */}
+                  <span className="hotDisc-bullet"></span>
+
+                  {/* 中间：标题 */}
+                  <span className="hotDisc-title">{disc.title()}</span>
+
+                  {/* 右侧：极简的评论数统计 */}
+                  <span className="hotDisc-stats">
+                    {icon('fas fa-comment-alt')} {disc.commentCount()}
+                  </span>
+                  
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
